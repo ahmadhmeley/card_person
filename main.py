0416655ed7086a46f48a424142514166c8410629
ahmadhmeley <87470619+ahmadhmeley@users.noreply.github.com>
2022-03-13 10:11:59 +0300

    api python
    api python

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, status, HTTPException
-from models import Id_queue
+from psycopg2.extras import RealDictCursor
+
+from models import s as model_s
 from database import sessionLocal
 from psycopg2._psycopg import connection as PSQLConnection
+import uvicorn
 import psycopg2
 
 app = FastAPI()
@@ -9,14 +12,13 @@ app = FastAPI()
 con = psycopg2.connect("dbname=abo_Mohammed user=postgres password=123456")
 
 
-def query2(query, parameter=(), db: PSQLConnection = con):
-    cur = db.cursor()
-    cur.callproc(query, parameter)
-    row = cur.fetchone()
-    cur.execute(f'FETCH ALL IN "{row[0]}"')
-    results = cur.fetchall()
-    db.close()
-    return results
+def querytable(query, parameter=(), db: con = None):
+        cur = db.cursor(cursor_factory=RealDictCursor)
+        cur.callproc(query, parameter)
+        results = cur.fetchall()
+        con.commit()
+        return results
+
 
 class Config:
     orm_mode = True
@@ -26,11 +28,19 @@ db = sessionLocal()
 
 
 @app.get('/card_person/{id}')
-def id_queue(id : str):
+def id_queue(id: str):
     try:
-        result = query2("view_id_queue",[id], db=con)
-        result_list = [Id_queue(card_id=i[1], civil_name=i[2], station_name=i[3], done=i[0]) for i in result]
-        print(result)
-        return {'view_id_queue': result_list}
+        result = querytable("view_id_queue", [id], db=con)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
+
+@app.get('/card_record/{id}')
+def id_queue(id: str):
+    try:
+        result = querytable("view_reception_queue_date", [id], db=con)
+        return result
+
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-from datetime import time , date
+from datetime import time, date
 
 from database import base
 from pydantic import BaseModel
 
 
-class Id_queue(BaseModel):
+class s(BaseModel):
     card_id: str
     civil_name: str
     station_name: str
@@ -12,11 +12,11 @@ class Id_queue(BaseModel):
 
 
 class reception_queue(BaseModel):
-    card_id: int
+    record_id: str
     civil_name: str
     station_name: str
     civil_date: date
     start_at: time
     end_at: time
-    times_num: int
+    times_num: str
     done: bool
